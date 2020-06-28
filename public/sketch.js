55ed6be3a1bb90a38713de88b1fe918e1f724f2b
LouisTheBest1231 <53878474+LouisTheBest1231@users.noreply.github.com>
2020-06-28 19:30:07 -0400

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,36 @@
+var socket;
+
+
+function setup() {
+	socket = io.connect('https://96.23.134.119:3000');
+	
+    createCanvas(500,500);
+    background(0);
+
+    socket.on('mouse', Draw);
+}
+
+function mouseDragged()
+{	
+	var data =
+	{
+		x:mouseX,
+		y:mouseY
+
+	}
+	socket.emit('mouse', data);
+
+	fill(color(0,0,255));
+	noStroke();
+	ellipse(mouseX, mouseY, 36,36);
+
+}
+
+function Draw(data)
+{
+	console.log("data" + data)
+	fill(255);
+	noStroke();
+	ellipse(data.x, data.y, 36,36);
+
+}

fab43bd2fe3b2d0551fe1f173525e23fd66d3d3f
LouisTheBest1231 <53878474+LouisTheBest1231@users.noreply.github.com>
2020-06-29 19:24:21 -0400

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -2,7 +2,7 @@ var socket;
 
 
 function setup() {
-	socket = io.connect('https://e6080e19b276.ngrok.io:8000');
+	socket = io.connect('http://192.168.1.1:8000');
 	
     createCanvas(500,500);
     background(0);
